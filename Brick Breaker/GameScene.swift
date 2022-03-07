e9f89f6110d24d2544842530743c464a3f6b0e10
Evan Zona <evanbzona@gmail.com>
2022-03-07 10:52:40 -0600

    Created brick object

--- Brick Breaker/GameScene.swift
+++ Brick Breaker/GameScene.swift
@@ -4,12 +4,14 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         func resetGame() {
-               // this stuff happens before each game starts
-               makeBall()
-           }
+            // this stuff happens before each game starts
+            makeBall()
+            makeBrick()
+        }
         // this stuff happens once (when the app opens)
         createBackground()
         resetGame()
@@ -51,16 +53,26 @@ class GameScene: SKScene {
         ball.physicsBody?.linearDamping = 0
         ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         addChild(ball)  // add ball object to the view
+        
         func makePaddle() {
-                paddle.removeFromParent()   // remove the paddle, if it exists
-                paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
-                paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
-                paddle.name = "paddle"
-                paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
-                paddle.physicsBody?.isDynamic = false
-                addChild(paddle)
-
+            paddle.removeFromParent()   // remove the paddle, if it exists
+            paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
+            paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+            paddle.name = "paddle"
+            paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+            paddle.physicsBody?.isDynamic = false
+            addChild(paddle)
+            
+            func makeBrick() {
+                brick.removeFromParent()    // remove the brick, if it exists
+                brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+                brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50
+                                         brick.name = "brick"
+                                         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+                                         brick.physicsBody?.isDynamic = false
+                                         addChild(brick)
             }
+        }
     }
 }
 
