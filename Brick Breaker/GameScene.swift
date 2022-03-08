003a60e1cbb7b1258278a593c596ef9760347426
Evan Zona <evanbzona@gmail.com>
2022-03-08 10:42:23 -0600

    Created paddle dynamic behavior

--- Brick Breaker/GameScene.swift
+++ Brick Breaker/GameScene.swift
@@ -1,21 +1,35 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
+    var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-        func resetGame() {
-            // this stuff happens before each game starts
-            makeBall()
-            makeBrick()
-        }
         // this stuff happens once (when the app opens)
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
         resetGame()
+        kickBall()
     }
+    
+    func resetGame() {
+        // this stuff happens before each game starts
+        makeBall()
+        makeBrick()
+        makePaddle()
+        makeLoseZone()
+        
+    }
+    
+    func kickBall() {
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+    }
+    
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
         for i in 0...1 {
@@ -30,6 +44,7 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
+    
     func makeBall() {
         ball.removeFromParent()     // remove the ball (if it exists)
         ball = SKShapeNode(circleOfRadius: 10)
@@ -53,26 +68,53 @@ class GameScene: SKScene {
         ball.physicsBody?.linearDamping = 0
         ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
         addChild(ball)  // add ball object to the view
-        
-        func makePaddle() {
-            paddle.removeFromParent()   // remove the paddle, if it exists
-            paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
-            paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
-            paddle.name = "paddle"
-            paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
-            paddle.physicsBody?.isDynamic = false
-            addChild(paddle)
-            
-            func makeBrick() {
-                brick.removeFromParent()    // remove the brick, if it exists
-                brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
-                brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50
-                                         brick.name = "brick"
-                                         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
-                                         brick.physicsBody?.isDynamic = false
-                                         addChild(brick)
-            }
+    }
+    
+    func makeLoseZone() {
+        loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+        loseZone.name = "loseZone"
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        loseZone.physicsBody?.isDynamic = false
+        addChild(loseZone)
+    }
+    
+    func makeBrick() {
+        brick.removeFromParent()    // remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+    }
+    
+    func makePaddle() {
+        paddle.removeFromParent()   // remove the paddle, if it exists
+        paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
         }
     }
 }
+
+
+
+
 
