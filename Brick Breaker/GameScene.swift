1cf06c2c0832028abc4fa346ab568e6d28335bd1
Evan Zona <evanbzona@gmail.com>
2022-03-09 10:53:58 -0600

    Added increasing ball velocity

--- Brick Breaker/GameScene.swift
+++ Brick Breaker/GameScene.swift
@@ -34,7 +34,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func kickBall() {
         ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
+        ball.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -5...5), dy: 5))
     }
     
     func updateLabels() {
@@ -169,6 +169,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 contact.bodyB.node == brick {
                 score += 1
                 updateLabels()
+                // increase ball velocity by 2%
+                ball.physicsBody!.velocity.dx = ball.physicsBody!.velocity.dx * CGFloat(1.02)
+                ball.physicsBody!.velocity.dy = ball.physicsBody!.velocity.dy * CGFloat(1.02)
                 if brick.color == .blue {
                     brick.color = .orange   // blue bricks turn orange
                 }
